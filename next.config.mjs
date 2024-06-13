cfdb962e4446f8007fa0793c7866f19b36b6739f
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2024-06-14 00:40:16 +0700

    fix: remotePatterns, rotation image, and notFound

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import { hostname } from "os";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 };
 
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div>
-      <h1>Sorry, Something Went Wrong!</h1>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
     </div>
   );
 }
