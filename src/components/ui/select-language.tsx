5ebc8e822f60155afce16224c617b7cc878c4d00
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2024-06-16 22:00:55 +0700

    fix: error package

--- src/components/ui/select-language.tsx
+++ src/components/ui/select-language.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { useLocale } from "next-intl";
 
 import {
   Select,
@@ -17,14 +16,13 @@ import { useRouter } from "next/navigation";
 export function SelectLanguage() {
   const [isPending, startTransition] = React.useTransition();
   const router = useRouter();
-  const localActive = useLocale();
   const onChange = (locale: string) => {
     startTransition(() => {
       router.replace(`/${locale}`, { scroll: false });
     });
   };
   return (
-    <Select defaultValue={localActive} onValueChange={onChange} disabled={isPending}>
+    <Select onValueChange={onChange} disabled={isPending}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Select Language" />
       </SelectTrigger>
