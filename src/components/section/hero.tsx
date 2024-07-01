7c706e0c64c5a2453d384e488bd05d0e6884e042
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2024-07-01 09:52:56 +0700

    style: add parallax image hero

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -1,14 +1,10 @@
-import Image from "next/image";
-
 export default function Hero() {
   return (
-    <div id="home" className="relative w-full h-screen flex flex-col justify-center items-center ">
-      <Image
-        alt="hero"
-        src={"/images/akita-1/akita-1.12.JPG"}
-        fill
-        className="object-cover grayscale"
-      />
+    <div
+      id="home"
+      className=" w-full h-screen flex flex-col justify-center items-center bg-fixed bg-cover bg-center bg-no-repeat relative grayscale"
+      style={{ backgroundImage: `url(/images/akita-1/akita-1.12.JPG)` }}
+    >
       <h1 className="absolute font-extrabold text-4xl  whitespace-nowrap md:text-7xl lg:text-8xl xl:text-9xl uppercase text-white bg-black blur-sm md:blur-md hover:blur-none ease-in-out transition-all duration-500 cursor-pointer">
         Akita Japan Fest
       </h1>
