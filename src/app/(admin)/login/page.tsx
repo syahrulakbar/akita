c960db8ba3c04d0651488706dba73fa11579b5da
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2024-07-02 16:29:34 +0700

    style: object contain icon

--- src/app/(admin)/login/page.tsx
+++ src/app/(admin)/login/page.tsx
@@ -11,13 +11,7 @@ export default function LoginPage() {
     <main className="flex items-center justify-center md:h-screen">
       <div className="space-y-2.6 relative mx-auto flex w-full max-w-[400px] flex-col p-4 md:-mt-32">
         <div className="flex items-center justify-center relative h-20 w-full  rounded-lg bg-slate-100 p-3 md:h-36">
-          <Image
-            alt="logo"
-            src={"/images/akita.png"}
-            width={200}
-            height={200}
-            objectFit="contain"
-          />
+          <Image alt="logo" src={"/images/akita.png"} fill className="object-contain " />
         </div>
         <LoginForm />
       </div>
