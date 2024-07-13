79a39d738ae0d7eedf8de1ec470d716b7a60d276
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2024-07-13 18:07:50 +0700

    fix: add react-use for window width n height

--- src/app/(user)/ticket/thanks/page.tsx
+++ src/app/(user)/ticket/thanks/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import useRedirectAfterSomeSeconds from "@/hooks/useRedirectAfterSomeSeconds";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
 
 export default function Page() {
   const { secondsRemaining } = useRedirectAfterSomeSeconds("/", 10);
+  const { width, height } = useWindowSize();
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, [loaded]);
   return (
     <>
-      <Confetti width={window.innerWidth} height={window.innerHeight} recycle={false} />
+      {loaded && <Confetti width={width} height={height} recycle={false} />}
       <div className="mx-auto overflow-x-hidden flex flex-col gap-3 items-center min-h-screen max-w-7xl px-5 py-10 lg:p-20 justify-center text-center">
         <p className="text-4xl font-bold uppercase">Arigathanks</p>
         <h2>We will contact you soon</h2>
